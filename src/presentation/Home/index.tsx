b3dfa967fb35e3efdb526b0ccfdc83d4e4909d71
Marlon Castro <msc@ecomp.poli.br>
2022-12-02 21:36:52 -0300

    chore: update all dependencies to last versions

--- src/presentation/Home/index.tsx
+++ src/presentation/Home/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-class Home extends React.PureComponent {
-  render() {
-    return (
-      <View>
-        <Text>Tela Home</Text>
-      </View>
-    );
-  }
+const Home = () => {
+  return (
+    <View>
+      <Text>Tela Home</Text>
+    </View>
+  );
 }
 
 export default Home
--- src/presentation/Login/index.tsx
+++ src/presentation/Login/index.tsx
@@ -1,21 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-class Login extends React.PureComponent {
-    componentWillMount = () => {
-
-    }
-
-    componentWillUnmount = () => {
-
-    }
-    render() {
-        return (
-            <View>
-                <Text>Tela de Login</Text>
-            </View>
-        );
-    }
+const Login = () => {
+  return (
+    <View>
+      <Text>Tela de Login</Text>
+    </View>
+  );
 }
 
 export default Login;
