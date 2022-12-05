3ab10935181bd9c07f16282b7b9b961ef214a8bc
Marlon Castro <msc@ecomp.poli.br>
2022-12-05 09:37:21 -0300

    fix: deleted lib deprecated of expo

--- src/presentation/Login/index.tsx
+++ src/presentation/Login/index.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { Text, View } from 'react-native'
+import { useFonts, Inter_400Regular } from '@expo-google-fonts/inter'
+
 import * as S from './styles'
 
-
 const Login = () => {
-  return (
-    <S.Container>
-      <Text>Tela de Login1</Text>
-    </S.Container>
-  );
+  let [fontsLoaded] = useFonts({
+    Inter_400Regular
+  })
+  if (!fontsLoaded) {
+    return <View />
+  } else {
+    return (
+      <S.Container>
+        <Text>Tela de Login1</Text>
+      </S.Container>
+    )
+  }
 }
 
 export default Login;
