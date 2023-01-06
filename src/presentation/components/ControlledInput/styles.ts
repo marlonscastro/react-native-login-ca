cc7ca548e470d165df565aaa71cb53d765f464c7
Marlon Castro <msc@ecomp.poli.br>
2023-01-05 22:23:55 -0300

    fix: styled od error sizing complete

--- src/presentation/components/ControlledInput/styles.ts
+++ src/presentation/components/ControlledInput/styles.ts
@@ -4,11 +4,12 @@ import { responsiveWidth } from 'react-native-responsive-dimensions'
 export const Error = styled.Text`
   color: #dc1637;
   font-size: 10px;
-  border-width: 1px;
-  border-color: #f00;
+  /* border-width: 1px;
+  border-color: #f00; */
   width: ${responsiveWidth(80)}px;
   text-align: center;
- 
+  margin-top: -5px;
+  margin-bottom: 10px;
 `
 
 export const Container = styled.View`
